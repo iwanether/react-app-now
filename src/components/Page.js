63cab6a8e76aa177f20e016faa30b012ee56b600
Global Chain <iwanether@gmail.com>
2019-04-29 12:10:34 +0800

    updated route

--- src/components/Page.js
+++ src/components/Page.js
@@ -10,8 +10,8 @@ const Page = props => {
     <div class="container mt-4">
       <h4>id is: {foo}</h4>
       <p>Try <a href="/page?id=123">/page?id=123</a> which should work</p>
-      <p>Try <a href="/page/123">/page/123</a> which should does not work (<a href="https://gist.github.com/iwanether/c65e02e54f31f65cfa1a5440a7a98791" target="_blank">
-                                                                           now.json</a>)</p>
+      <p>Try <a href="/page/123">/page/123</a> which should does not work</p>
+      <p><a href="https://route-app.iwanether.now.sh/_src" target="_blank">Now public _src</a></p>
 
 
 
